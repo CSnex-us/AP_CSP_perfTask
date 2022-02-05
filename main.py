eea80ba5fab873cf1a486d94fc695da11781d449
Stephen Maher <srqcoder@gmail.com>
2022-02-05 11:13:01 -0500

    Initial Data Structure for Question Bank

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+'''
+    Question Bank Server
+    Based on matching Student Profiency and Question Difficulty
+'''
+
 print("Hello AP CSC Perfomrance Task Project")
+
+# TODO  Create DataSet:  a) Students  b) Question Bank
+#           student: name, progress  
+
+# TODO  Statistical Range of 70% Success Rate
+
+# TODO  How to connect answer to question
+#           def getQuestion(student, standard):
+#           choose appropriate question
+#            
+
+qBank = [(1,"question 1", "answer 1.1", "answer 1.2"), (2,"question 2", "answer 2.1", "answer 2.2")]
+print(qBank[1][0])
