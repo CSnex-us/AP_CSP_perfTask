efaf47e07e276151211bee88993d8762ce3179e7
Stephen Maher <srqcoder@gmail.com>
2022-02-05 11:15:42 -0500

    Initial Data Structure for Question Bank

---
+++ calendar.py
@@ -0,0 +1,3 @@
+print("I've been paying attention, and I've go this one. It has to do \
+           with how many of those special characters are in the text. \
+           that special character is the \n sequence")
